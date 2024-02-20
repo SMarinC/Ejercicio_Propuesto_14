23838d43e3a05e3a4432d5a92161d72230417fd8
Santiago Marin Cardona <marincardona.santiago@hotmail.com>
2024-02-20 17:26:45 -0500

    Initial commit

---
+++ src/ejercicio_propuesto_14/Ejercicio_Propuesto_14.java
@@ -0,0 +1,16 @@
+
+package ejercicio_propuesto_14;
+
+public class Ejercicio_Propuesto_14 {
+
+    public static void main(String[] args) {
+        double num, cuadrado, cubo;
+        num = 7;
+        cuadrado = Math.pow(num, 2);
+        cubo = Math.pow(num, 3);
+        System.out.println("El numero es: " + num);
+        System.out.println("El cuadrado del numero es: "+ cuadrado);
+        System.out.println("El cubo del numerto es: " + cubo);
+    }
+    
+}
